abe9a37dbf864f2888b81f1a2988671d7f6f4788
kai <kai@kaiconsulting.co>
2022-01-28 19:03:54 -0700

    updated category routes with put

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -54,11 +54,37 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-
+  try {
+    const categoryUpdate = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryUpdate) {
+      res.status(404).json({ message: 'No category with this id' });
+    }
+    res.status(200).json(categoryUpdate);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (e) {
+    res.json(e);
+  }
 });
 
 module.exports = router;
