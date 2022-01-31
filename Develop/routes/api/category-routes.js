7eb71becaf69bfa098ef1925c5fb2306483629eb
kai <kai@kaiconsulting.co>
2022-01-31 15:03:16 -0700

    fixed category route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -54,23 +54,22 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  const { category_name } = req.body;
+  // const { category_name } = req.body;
   try {
-    const categoryUpdate = await Category.update(req.body, {
-        category_name,
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
-    });
+    await Category.update(req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
     // if (!categoryUpdate) {
     //   res.status(404).json({ message: 'No category with this id' });
     // }
     const updatedCategory = await Category.findByPk(req.params.id);
     res.status(200).json(updatedCategory);
   } catch (e) {
-    res.status(500).json(e);
+    res.status(400).json(e);
   }
 });
 
